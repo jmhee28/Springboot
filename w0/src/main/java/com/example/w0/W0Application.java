b321ac588d703a63feb3f03d7498617eadcb063d
jmhee28 <mhee3428@gmail.com>
2022-05-23 14:12:33 +0900

    skku_sw_coaching

---
+++ w0/src/main/java/com/example/w0/W0Application.java
@@ -0,0 +1,13 @@
+package com.example.w0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W0Application.class, args);
+	}
+
+}
---
+++ w0/src/test/java/com/example/w0/W0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
